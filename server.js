82eaaa8dd4371d09bf3c4b30bded427dacf33a19
Donaldo Martinez <do073496@ucf.edu>
2025-04-08 12:57:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -51,6 +51,26 @@ app.post('/api/login', async (req, res, next) => {
     res.status(200).json(ret);
 });
 
+app.post('/api/login2', async (req, res, next) => {
+    // incoming: login, password
+    // outgoing: id, firstName, lastName, error
+    var error = '';
+    const { login, password } = req.body;
+    const db = client.db();
+    const results = await
+        db.collection('Users').find({ Login: login, Password: password }).toArray();
+    var id = -1;
+    var fn = '';
+    var ln = '';
+    if (results.length > 0) {
+        id = results[0].UserId;
+        fn = results[0].FirstName;
+        ln = results[0].LastName;
+    }
+    var ret = { id: id, firstName: fn, lastName: ln, error: '' };
+    res.status(200).json(ret);
+});
+
 app.post('/webhook', (req, res) => {
     console.log('🚨 Received webhook POST request!');
     const signature = req.headers['x-hub-signature-256'];
