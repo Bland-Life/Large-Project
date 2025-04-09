4c3ea27c47d4513105b9804816f8c4f755a4e6eb
Ash Hanzelka <as117448@ucf.edu>
2025-04-09 17:19:52 -0400

    homepage completion

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import videoBack from '../assets/clouds.mp4';
+import logoImage from '../assets/logoFull.png';
 import './HomePage.css'
+
 
 const HomePage = () => {
     return (
-        <div className="background-content">
+        <div className="page-content">
             <video src={videoBack} autoPlay loop muted />
             <div className="content-box">
-                <h2 className="subheading">Whether You're on a Plane or on a Train...</h2>
-                <h3 className="tagline">We've Got You Covered!</h3>
-                <h1 className="main-heading">Oh, the Places You'll Go!</h1>
-                <div className="button-container">
-                    <button className="button">Login!</button>
-                    <button className="button">Sign Up!</button>
-                </div>
+                {/* <h2 className="subheading">Whether You're on a Plane or on a Train...</h2> */}
+                <img src={logoImage} alt="Logo" className = "logo" />
+                {/* <h3 className="tagline">We've Got You Covered!</h3>
+                <h1 className="main-heading">Oh, the Places You'll Go!</h1> */}
+            </div>
+            <div className="button-container"> 
+                <button className="button">Login!</button>
+                <button className="button">Sign Up!</button>
             </div>
         </div>
     );
