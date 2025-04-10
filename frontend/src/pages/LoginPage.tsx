8251d9457001b4097faa966089c44fa5535947cd
Donaldo Martinez <do073496@ucf.edu>
2025-04-10 13:50:20 -0400

    Update LoginPage.tsx

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,11 +1,13 @@
-import PageHeader from '../components/PageHeader.tsx';
 import Login from '../components/Login.tsx';
+import videoBack from '../assets/clouds.mp4';
+import logoImage from '../assets/logoFull.png';
+import '../css/HomePage.css';
 
 const LoginPage = () =>
 {
 	return (
-		<div>
-			<PageHeader />
+		<div className='page-content'>
+			<video src={videoBack} autoPlay loop muted />
 			<Login />
 		</div>
 	);
