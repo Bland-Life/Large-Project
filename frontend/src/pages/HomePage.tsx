c36c5f15f9542172b0be7bdd1646cb7dc8e1d0bf
Bland-Life <136954372+Bland-Life@users.noreply.github.com>
2025-04-09 21:11:30 -0400

    Update HomePage.tsx
    Minor change to the alt description of the website logo to promote accessibility.

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -10,7 +10,7 @@ const HomePage = () => {
             <video src={videoBack} autoPlay loop muted />
             <div className="content-box">
                 {/* <h2 className="subheading">Whether You're on a Plane or on a Train...</h2> */}
-                <img src={logoImage} alt="Logo" className = "logo" />
+                <img src={logoImage} alt="The website logo. Oh The Places You'll Go!" className = "logo" />
                 {/* <h3 className="tagline">We've Got You Covered!</h3>
                 <h1 className="main-heading">Oh, the Places You'll Go!</h1> */}
             </div>
