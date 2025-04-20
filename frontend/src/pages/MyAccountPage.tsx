b33dde687c826ac154a7472de6aef49a67037c31
Ash Hanzelka <as117448@ucf.edu>
2025-04-20 19:48:28 -0400

    main account page css

--- frontend/src/pages/MyAccountPage.tsx
+++ frontend/src/pages/MyAccountPage.tsx
@@ -17,10 +17,10 @@ const MyAccountPage = () => (
     <section className="travel-stats">
       <h2>Travel Stats</h2>
       <div className="stat-grid">
-        <span><strong>7</strong> / 7 continents</span>
-        <span><strong>8</strong> / 195 countries</span>
-        <span><strong>30</strong> / 50 states</span>
-        <span><strong>22</strong> / 87 megacities</span>
+        <span><strong>7</strong> / 7 continents</span>
+        <span><strong>8</strong> / 195 countries</span>
+        <span><strong>30</strong> / 50 states</span>
+        <span><strong>22</strong> / 87 megacities</span>
       </div>
     </section>
   </div>
