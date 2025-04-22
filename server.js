c648953a38aa22f85172505186bbd6e6808a3f1d
Christian Solanet <christiansolanet@gmail.com>
2025-04-22 18:46:29 -0400

    one more line for file limit

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(cors());
 app.use('/webhook', express.raw({ type: 'application/json' }));
 app.use(bodyParser.json());
 app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 
 app.use((req, res, next) => {
