88c66bbe3617460ce166c56066c94a4533cd452e
Christian Solanet <christiansolanet@gmail.com>
2025-04-23 19:55:44 -0400

    Utils update

--- frontend/src/utils/utils.tsx
+++ frontend/src/utils/utils.tsx
@@ -1,11 +1,8 @@
 export function formatDate(date : Date) : string {
-    const formattedDate = `${(date.getMonth() + 1)
-        .toString()
-        .padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}/${date
-        .getFullYear()
-        .toString()}`;
-        
-    return formattedDate;
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const day = String(date.getDate()).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${month}/${day}/${year}`;
 }
 
 export function getImageString(imageFile : File) : string {
