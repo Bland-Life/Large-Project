7719d8f509d3ce8bc1c408134d0169decb6a1646
Benjamin_Titus <160093052+Benjamintitus03@users.noreply.github.com>
2025-04-23 18:05:34 -0400

    Update App.tsx
    updated in relation to the layout.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,49 @@
-// src/App.tsx
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./css/App.css";
 
-/* existing pages */
-import HomePage          from "./pages/HomePage";
-import LoginPage         from "./pages/LoginPage";
-import SignupPage        from "./pages/SignUpPage";
-import MyAccount         from "./pages/MyAccountPage";
-import WhereIveBeenPage  from "./pages/WhereIveBeenPage";
-import WhereImGoingPage  from "./pages/WhereImGoingPage";
-import JoinMePage        from "./pages/JoinMePage";
-import TravelToolsPage   from "./pages/TravelToolsPage";
-import UserProfilePage   from "./pages/UserProfilePage";
+// Existing pages
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignUpPage";
+import MyAccount from "./pages/MyAccountPage";
+import WhereIveBeenPage from "./pages/WhereIveBeenPage";
+import WhereImGoingPage from "./pages/WhereImGoingPage";
+import JoinMePage from "./pages/JoinMePage";
+import TravelToolsPage from "./pages/TravelToolsPage";
+import UserProfilePage from "./pages/UserProfilePage";
 
-/* your new map component */
-import MapExplorer       from "./components/MapExplorer";
+// Map component
+import MapExplorer from "./components/MapExplorer";
+
+// Layout component that includes the NavBar
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* public routes */}
-        <Route path="/"            element={<HomePage />} />
-        <Route path="/Login"       element={<LoginPage />} />
-        <Route path="/Signup"      element={<SignupPage />} />
+        {/* All public routes are inside the Layout */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Login" element={<LoginPage />} />
+          <Route path="/Signup" element={<SignupPage />} />
 
-        {/* account dashboard */}
-        <Route path="/MyAccount"   element={<MyAccount />} />
+          {/* Account dashboard */}
+          <Route path="/MyAccount" element={<MyAccount />} />
 
-        {/* sub‑sections */}
-        <Route path="/WhereIveBeen" element={<MyAccount />} />
-        <Route path="/WhereImGoing" element={<WhereImGoingPage />} />
-        <Route path="/JoinMe"       element={<JoinMePage />} />
-        <Route path="/TravelTools"  element={<TravelToolsPage />} />
-        <Route path="/Profile"      element={<UserProfilePage />} />
+          {/* Sub-sections */}
+          <Route path="/WhereIveBeen" element={<WhereIveBeenPage />} />
+          <Route path="/WhereImGoing" element={<WhereImGoingPage />} />
+          <Route path="/JoinMe" element={<JoinMePage />} />
+          <Route path="/TravelTools" element={<TravelToolsPage />} />
+          <Route path="/Profile" element={<UserProfilePage />} />
 
-        {/* drill‑down map routes (optional direct access) */}
-        <Route path="/map"                          element={<MapExplorer />} />
-        <Route path="/map/:countryCode"             element={<MapExplorer />} />
-        <Route path="/map/:countryCode/:cityId"     element={<MapExplorer />} />
+          {/* Map Explorer with drill-down routes */}
+          <Route path="/map" element={<MapExplorer />} />
+          <Route path="/map/:countryCode" element={<MapExplorer />} />
+          <Route path="/map/:countryCode/:cityId" element={<MapExplorer />} />
+        </Route>
       </Routes>
     </Router>
   );
