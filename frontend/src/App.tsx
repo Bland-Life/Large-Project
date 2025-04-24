669d51cc9466e18e5c77a0363506fca69b3aca22
Bland-Life <136954372+Bland-Life@users.noreply.github.com>
2025-04-24 10:15:10 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,24 +24,26 @@ function App() {
   return (
     <Router>
       <Routes>
+
+
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Signup" element={<SignupPage />} />
         {/* Wrap all routes with Layout so nav/sidebar shows */}
-        <Route>  
-          <Route path="/"                         element={<HomePage />} />
-          <Route path="/Login"                    element={<LoginPage />} />
-          <Route path="/Signup"                   element={<SignupPage />} />
-          <Route path="/test-map"                 element={<TestMapPage />} />
-          <Route path="/MyAccount"                element={<MyAccount />} />
+        <Route element={<Layout />}>
+          <Route path="/test-map" element={<TestMapPage />} />
+          <Route path="/MyAccount" element={<MyAccount />} />
 
           {/* Sub-sections */}
-          <Route path="/WhereIveBeen"             element={<WhereIveBeenPage />} />
-          <Route path="/WhereImGoing"             element={<WhereImGoingPage />} />
-          <Route path="/JoinMe"                   element={<JoinMePage />} />
-          <Route path="/TravelTools"              element={<TravelToolsPage />} />
-          <Route path="/Profile"                  element={<UserProfilePage />} />
+          <Route path="/WhereIveBeen" element={<WhereIveBeenPage />} />
+          <Route path="/WhereImGoing" element={<WhereImGoingPage />} />
+          <Route path="/JoinMe" element={<JoinMePage />} />
+          <Route path="/TravelTools" element={<TravelToolsPage />} />
+          <Route path="/Profile" element={<UserProfilePage />} />
 
           {/* Map Explorer with drill-down */}
-          <Route path="/map"                      element={<MapExplorer />} />
-          <Route path="/map/:countryCode"         element={<MapExplorer />} />
+          <Route path="/map" element={<MapExplorer />} />
+          <Route path="/map/:countryCode" element={<MapExplorer />} />
           <Route path="/map/:countryCode/:cityId" element={<MapExplorer />} />
         </Route>
       </Routes>
